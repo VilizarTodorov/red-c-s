f1a43f67b8ece005e31b1100d2e11b9c6fc024b7
VilizarTodorov <todorovvilizar@gmail.com>
2021-03-09 19:56:23 +0200

    post resolver

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import mikroOrmConfig from "./mikro-orm.config";
@@ -5,6 +6,7 @@ import express from "express";
 import { PORT } from "./constants";
 import { ApolloServer } from "apollo-server-express";
 import { HelloResolver } from "./resolvers/hello";
+import { PostResolver } from "./resolvers/Post";
 import { buildSchema } from "type-graphql";
 
 const main = async () => {
@@ -14,7 +16,8 @@ const main = async () => {
   const app = express();
 
   const apolloServer = new ApolloServer({
-    schema: await buildSchema({ resolvers: [HelloResolver], validate: false }),
+    schema: await buildSchema({ resolvers: [HelloResolver, PostResolver], validate: false }),
+    context: () => ({ em: orm.em }),
   });
 
   apolloServer.applyMiddleware({ app });
